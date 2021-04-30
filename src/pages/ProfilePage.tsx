7d8ea6ac3880a363d61305a9d3775dd6f74aeb65
Victor Shleifman <v.shleifman@gmail.com>
2021-04-30 08:45:31 +0100

    type fix for Profile Page

--- src/pages/ProfilePage.tsx
+++ src/pages/ProfilePage.tsx
@@ -10,23 +10,26 @@ const Container = styled.div`
 const ProfilePage = () => {
   const dispatch = useDispatch();
   const user = useSelector(userSelector).userData;
-
-  return (
-    <Container>
-      <div>
-        <p>First Name: {user?.firstName}</p>
-        <p>Last Name: {user?.lastName}</p>
-        <p>Email: {user?.email}</p>
-      </div>
-      <button
-        onClick={() => {
-          dispatch(signoutThunk());
-        }}
-      >
-        Signout
-      </button>
-    </Container>
-  );
+  if (!user) {
+    return <Container></Container>;
+  } else {
+    return (
+      <Container>
+        <div>
+          <p>First Name: {user.firstName}</p>
+          <p>Last Name: {user.lastName}</p>
+          <p>Email: {user.email}</p>
+        </div>
+        <button
+          onClick={() => {
+            dispatch(signoutThunk());
+          }}
+        >
+          Signout
+        </button>
+      </Container>
+    );
+  }
 };
 
 export default ProfilePage;
