99048126084f2a13dfbfca60719a803ca9415bcf
Victor Shleifman <v.shleifman@gmail.com>
2021-04-30 08:57:33 +0100

    booking-apartments

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,28 @@
-import Navbar from "./components/Navbar/Navbar";
-import { Route, Switch, useLocation } from "react-router-dom";
-import Home from "./pages/Home";
-import Contacts from "pages/Contacts";
-import Apartments from "components/ApartmentList/Apartments";
-import ProfilePage from "pages/ProfilePage";
-import { useEffect, useRef } from "react";
-import { useDispatch } from "react-redux";
-import { tryAutoSignin } from "reducers/AuthSlice";
-import styled, { createGlobalStyle } from "styled-components";
-import Welcome from "components/Navbar/Welcome";
-import AuthForm from "components/Authentication/AuthForm";
-import AddApartment from "components/ApartmentList/AddApartment";
+import Navbar from './components/Navbar/Navbar';
+import { Route, Switch, useLocation } from 'react-router-dom';
+import Home from './pages/Home';
+import Contacts from 'pages/Contacts';
+import ProfilePage from 'pages/ProfilePage';
+import { useEffect, useRef } from 'react';
+import { useDispatch } from 'react-redux';
+import { tryAutoSignin } from 'reducers/AuthSlice';
+import styled, { createGlobalStyle } from 'styled-components';
+import Welcome from 'components/Navbar/Welcome';
+import AuthForm from 'components/Authentication/AuthForm';
+import ApartmentsSwitch from 'components/ApartmentList/ApartmentsSwitch';
+import rigaPanorama from 'images/Riga_panorama.jpg';
+import AddApartment from 'components/ApartmentList/AddApartment';
 
 const GlobalFonts = createGlobalStyle`
 body {
-    font-family: 'Playfair Display', serif;
+	font-family: 'Playfair Display', serif;
 }
 
 `;
 const Container = styled.div`
   display: grid;
-  grid-template: "nav nav nav" auto " . switch . " auto " . . ." 5em / 1fr 3fr 1fr;
-  font-family: "Playfair Display";
+  grid-template: 'nav nav nav' auto ' . switch . ' auto ' . . .' 5em / 1fr 3fr 1fr;
+  font-family: 'Playfair Display';
   font-size: 1.8vh;
 `;
 
@@ -34,7 +35,7 @@ const StSwitch = styled.div`
 const NavBackground = styled.header`
   grid-area: nav;
   background-image: linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)),
-    url("https://upload.wikimedia.org/wikipedia/commons/b/b8/Riga_panorama.jpg");
+    url('${rigaPanorama}');
   background-size: cover;
   background-position: center;
   background-attachment: fixed;
@@ -55,9 +56,9 @@ const App = () => {
   return (
     <Container>
       <GlobalFonts />
-      <NavBackground height={location.pathname === "/" ? "100vh" : "10vh"}>
+      <NavBackground height={location.pathname === '/' ? '100vh' : '10vh'}>
         <Navbar />
-        {location.pathname === "/" ? <Welcome reference={ref} /> : null}
+        {location.pathname === '/' ? <Welcome reference={ref} /> : null}
       </NavBackground>
 
       <StSwitch>
@@ -65,7 +66,7 @@ const App = () => {
           <Route exact path="/" render={() => <Home reference={ref} />} />
           <Route path="/auth" component={AuthForm} />
           <Route path="/contacts" component={Contacts} />
-          <Route path="/apartments" component={Apartments} />
+          <Route path="/apartments" component={ApartmentsSwitch} />
           <Route path="/add_apartment" component={AddApartment} />
           <Route path="/profile" component={ProfilePage} />
         </Switch>
